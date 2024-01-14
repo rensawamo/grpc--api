06947d011cb134e184fbba591353e1839d2ec251
rensawamo <ren0112ngzk@gmail.com>
2024-01-14 12:13:17 +0900

    送受信のIDの分岐によるdeadlockの対策

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -29,8 +29,8 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 		return err
 	}
 	q := New(tx)
-	err = fn(q)   
-	if err != nil { 
+	err = fn(q)
+	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
@@ -53,15 +53,13 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
-
 // 一方の口座から他方の口座へ送金を行う
 // データベーストランザクション 口座エントリを追加して 口座の残高を更新
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
-	err := store.execTx(ctx, func(q *Queries) error {  //トランザクション
+	err := store.execTx(ctx, func(q *Queries) error { //トランザクション
 		var err error
-
 
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
@@ -88,33 +86,84 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID) 
+		// account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
 		// if err != nil {
 		// 	return err
 		// }
 
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams {
-			ID: arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
+		// deadlock 対策
+		// トランザクションのデッドロックは 送信者と受信者の順番が大切
+		// 受け手と送りての 順番の差をしっかり指定するといい
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.FromAccountID,
+				Amount: -arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+
+			// account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+			// if err != nil {
+			// 	return err
+			// }
+
+			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.ToAccountID,
+				Amount: arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+		} else {
+			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.ToAccountID,
+				Amount: arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
+
+			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.FromAccountID,
+				Amount: -arg.Amount,
+			})
+			if err != nil {
+				return err
+			}
 		}
 
-		// account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID) 
-		// if err != nil {
-		// 	return err
-		// }
-
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams {
-			ID: arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
+		
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 
 		return nil
 	})
 	return result, err
+}
+
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return Account{}, Account{}, err
+	}
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
 }
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -8,34 +8,43 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTransferTx(t *testing.T) {
+
+
+func TestTransferTxDeadlock(t *testing.T) {
 	store := NewStore(testDB)
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
-	n := 5
+	// pq: deadlock detected
+	n := 10
 	amount := int64(10)
 
 	errs := make(chan error)
-	results := make(chan TransferTxResult)
+	// results := make(chan TransferTxResult)
 
 	// run n concurrent transfer transaction
 	for i := 0; i < n; i++ {
-		
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i % 2 == 1 { // 並行処理の回数を増やして 半分は 送信の 人を逆転
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
 		go func() {
 			ctx := context.Background()  //valueの追加 キーは 空
-			result, err := store.TransferTx(ctx, TransferTxParams{
-				FromAccountID: account1.ID,
-				ToAccountID:   account2.ID,
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
 				Amount:        amount,
 			})
 
 			errs <- err
-			results <- result
+			// results <- result
 		}()
 	}
-	existed := make(map[int]bool)
+	// existed := make(map[int]bool)
 	// check results
 	for i := 0; i < n; i++ {
 		err := <-errs
@@ -47,65 +56,65 @@ func TestTransferTx(t *testing.T) {
 		// FOR NO KEY UPDATE;
 		// キーを毎回作成しないことを明示すると デッドロックがおこない
 
-		result := <-results
-		require.NotEmpty(t, result)
+		// result := <-results
+		// require.NotEmpty(t, result)
 
 		// check transfer
-		transfer := result.Transfer
-		require.NotEmpty(t, transfer)
-		require.Equal(t, account1.ID, transfer.FromAccountID) // 送った人
-		require.Equal(t, account2.ID, transfer.ToAccountID)   // 受け取った人
-		require.Equal(t, amount, transfer.Amount)
-		require.NotZero(t, transfer.ID)
-		require.NotZero(t, transfer.CreatedAt)
+		// transfer := result.Transfer
+		// require.NotEmpty(t, transfer)
+		// require.Equal(t, account1.ID, transfer.FromAccountID) // 送った人
+		// require.Equal(t, account2.ID, transfer.ToAccountID)   // 受け取った人
+		// require.Equal(t, amount, transfer.Amount)
+		// require.NotZero(t, transfer.ID)
+		// require.NotZero(t, transfer.CreatedAt)
 
-		_, err = store.GetTransfer(context.Background(), transfer.ID)
-		require.NoError(t, err)
+		// _, err = store.GetTransfer(context.Background(), transfer.ID)
+		// require.NoError(t, err)
 
-		// check entries
-		fromEntry := result.FromEntry
-		require.NotEmpty(t, fromEntry)
-		require.Equal(t, account1.ID, fromEntry.AccountID)
-		require.Equal(t, -amount, fromEntry.Amount)
-		require.NotZero(t, fromEntry.ID)
-		require.NotZero(t, fromEntry.CreatedAt) //決済日付のテストは0 でない
+		// // check entries
+		// fromEntry := result.FromEntry
+		// require.NotEmpty(t, fromEntry)
+		// require.Equal(t, account1.ID, fromEntry.AccountID)
+		// require.Equal(t, -amount, fromEntry.Amount)
+		// require.NotZero(t, fromEntry.ID)
+		// require.NotZero(t, fromEntry.CreatedAt) //決済日付のテストは0 でない
 
-		_, err = store.GetEntry(context.Background(), fromEntry.ID)
-		require.NoError(t, err)
+		// _, err = store.GetEntry(context.Background(), fromEntry.ID)
+		// require.NoError(t, err)
 
-		toEntry := result.ToEntry
-		require.NotEmpty(t, toEntry)
-		require.Equal(t, account2.ID, toEntry.AccountID)
-		require.Equal(t, amount, toEntry.Amount)
-		require.NotZero(t, toEntry.ID)
-		require.NotZero(t, toEntry.CreatedAt)
+		// toEntry := result.ToEntry
+		// require.NotEmpty(t, toEntry)
+		// require.Equal(t, account2.ID, toEntry.AccountID)
+		// require.Equal(t, amount, toEntry.Amount)
+		// require.NotZero(t, toEntry.ID)
+		// require.NotZero(t, toEntry.CreatedAt)
 
-		_, err = store.GetEntry(context.Background(), toEntry.ID)
-		require.NoError(t, err)
+		// _, err = store.GetEntry(context.Background(), toEntry.ID)
+		// require.NoError(t, err)
 
-		// check accounts
-		fromAccount := result.FromAccount
-		require.NotEmpty(t, fromAccount)
-		require.Equal(t, account1.ID, fromAccount.ID)
+		// // check accounts
+		// fromAccount := result.FromAccount
+		// require.NotEmpty(t, fromAccount)
+		// require.Equal(t, account1.ID, fromAccount.ID)
 
-		toAccount := result.ToAccount
-		require.NotEmpty(t, toAccount)
-		require.Equal(t, account2.ID, toAccount.ID)
+		// toAccount := result.ToAccount
+		// require.NotEmpty(t, toAccount)
+		// require.Equal(t, account2.ID, toAccount.ID)
 
-		// check balances
-		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+		// // check balances
+		// fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
 
-		diff1 := account1.Balance - fromAccount.Balance
-		diff2 := toAccount.Balance - account2.Balance
-		require.Equal(t, diff1, diff2) //ここで dbの更新をgoroutineで回しているから 値が保証されない
-		require.True(t, diff1 > 0)
-		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount, ..., n * amount
+		// diff1 := account1.Balance - fromAccount.Balance
+		// diff2 := toAccount.Balance - account2.Balance
+		// require.Equal(t, diff1, diff2) //ここで dbの更新をgoroutineで回しているから 値が保証されない
+		// require.True(t, diff1 > 0)
+		// require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount, ..., n * amount
 
-		k := int(diff1 / amount)
-		require.True(t, k >= 1 && k <= n)
-		require.NotContains(t, existed, k)
-		existed[k] = true
-	}
+		// k := int(diff1 / amount)
+		// require.True(t, k >= 1 && k <= n)
+		// require.NotContains(t, existed, k)
+		// existed[k] = true
+	}							
 
 	// check the final updated balance
 	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
@@ -116,7 +125,9 @@ func TestTransferTx(t *testing.T) {
 
 	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
 
-	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
-	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+	// require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
 
 }
